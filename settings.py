7e08ecfd59120c9a385d85968f977eec2d880a77
Tim Golden <mail@timgolden.me.uk>
2012-03-04 16:43:23 +0000

    Add a link for the ldnpydojo Twitter account

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 SITENAME="The London Python Dojo"
-#~ DISPLAY_PAGES_ON_MENU=False
 TIMEZONE="UTC"
 THEME="./themes/notmyidea-dojo"
 GITHUB_URL="https://github.com/ldnpydojo/ldnpydojo.org.uk"
@@ -7,4 +6,5 @@ GITHUB_URL="https://github.com/ldnpydojo/ldnpydojo.org.uk"
 LINKS = [
   ('GitHub', 'https://github.com/ldnpydojo/'),
   ("Flickr", "http://www.flickr.com/photos/75748749@N08/"),
+  ("Twitter", "https://twitter.com/#!/ldnpydojo"),
 ]
