1c6df51a4fb59315a8edcfd40447467741faf899
Tim Golden <mail@timgolden.me.uk>
2012-03-05 14:01:06 +0000

    Complete work on flask version

--- rebuild/rebuild.py
+++ rebuild/rebuild.py
@@ -5,18 +5,20 @@ from flask import Flask
 app = Flask(__name__)
 import pelican
 
-WC_FILEPATH = "/home/tgolden/work-in-progress/ldnpydojo.org.uk"
 WC_FILEPATH = r"C:\work-in-progress\ldnpydojo.org.uk"
+WC_FILEPATH = "/home/tgolden/work-in-progress/ldnpydojo.org.uk"
 
 @app.route ('/')
 def rebuild ():
+  output = []
   os.chdir (WC_FILEPATH)
   with open ("stdout.log", "w") as stdout:
-    subprocess.check_call (["git", "pull"], stdout=stdout)
-  yield "Git pulled"
+    subprocess.check_call (["git", "pull"], stdout=stdout, shell=True)
+  output.append ("Git pulled")
   sys.argv[1:] = ['-s', 'settings.py', 'site']
   pelican.main ()
-  yield "Pelican rebuild complete"
+  output.append ("Pelican rebuild complete")
+  return "\n".join (output)
 
 if __name__ == '__main__':
-  app.run ()
+  app.run (debug=True)
